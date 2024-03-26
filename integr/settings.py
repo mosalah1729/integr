c7d33a2ad67489946b96c605785053301c3dbf0c
mosalah1729 <109469555+mosalah1729@users.noreply.github.com>
2024-03-26 15:05:39 +0530

    Update settings.py

--- integr/settings.py
+++ integr/settings.py
@@ -98,7 +98,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'https://integr-q2q52dv2j-mosalah1729s-projects.vercel.app',
-    https://vercel.com/mosalah1729s-projects/integr/,
+    'https://vercel.com/mosalah1729s-projects/integr/',
 ]
 
 ROOT_URLCONF = 'integr.urls'
