11f5120d0e87648ad601836f8482f65bc8fa8913
mosalah1729 <mohammedsalah2048@gmail.com>
2024-08-03 14:18:27 +0530

    my_view check removed

--- Frontend/src/User/User_comp/Nowshow.js
+++ Frontend/src/User/User_comp/Nowshow.js
@@ -21,21 +21,7 @@ function Upcoming(props) {
   const { movieLoc } = props;
   const { userName } = props;
   const [movies, setMovies] = useState([]);
-  const [response, setResponse] = useState('');
-
-  useEffect(() => {
-    // Function to fetch data from the Django backend
-    const fetchData = async () => {
-      try {
-        const result = await axios.get('/articles/my_view/'); // Replace with your Django server URL
-        setResponse(result.data.text);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
+ 
 
   useEffect(() => {
     const csrftoken = getCookie('csrftoken');
