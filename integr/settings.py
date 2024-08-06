11abc849397b0c4fb0bc159cb953c185a828e01c
mosalah1729 <mohammedsalah2048@gmail.com>
2024-08-06 21:24:15 +0530

    cors credential

--- integr/settings.py
+++ integr/settings.py
@@ -90,7 +90,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     
 ]
-
+CORS_ALLOW_CREDENTIALS = False
 CORS_ALLOW_ALL_ORIGINS = True
 
 # CORS_ALLOWED_ORIGINS = [
