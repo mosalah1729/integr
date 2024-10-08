9e1a22ad8e3e962d1577f91c738c0c1db085fddb
mosalah1729 <mohammedsalah2048@gmail.com>
2024-10-08 12:48:22 +0530

    movieLoc added in Theatres.js

--- Frontend/src/User/page/Theatres.js
+++ Frontend/src/User/page/Theatres.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Header from '../User_head/User_head'
 import Theatre from '../User_comp/Theatre'
+import { useLocation } from 'react-router-dom';
+
+
 function Theatres() {
+  const location = useLocation();
+const movieLoc = location.state?.movieLoc;
+console.log('theatress', movieLoc);
   return (
     <div>
       <Header/>
-      <Theatre/>
+      <Theatre movieLoc={movieLoc}/>
     </div>
   )
 }
